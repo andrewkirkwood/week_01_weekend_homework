336db302639b44d267ca5d244199e528a71718d3
Andrew Kirkwood <andrewkirkwood90@gmail.com>
2019-11-22 14:31:28 +0000

    6th function passed

--- start_point/pet_shop.rb
+++ start_point/pet_shop.rb
@@ -13,3 +13,14 @@ end
 def pets_sold(pet_shop)
   return pet_shop[:admin][:pets_sold]
 end
+
+def increase_pets_sold(pet_shop, pet_qty_sold_now)
+  return pet_shop[:admin][:pets_sold] += pet_qty_sold_now
+end
+
+def stock_count(pet_shop)
+  stock_total = 0
+  for pets in pet_shop
+   return stock_total += pet_shop[:pets].count
+ end
+end
