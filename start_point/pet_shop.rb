44e1c29e777d2b5683cdf8a421bfa076a3e233e7
Andrew Kirkwood <andrewkirkwood90@gmail.com>
2019-11-22 14:07:58 +0000

    third function written

--- start_point/pet_shop.rb
+++ start_point/pet_shop.rb
@@ -1,7 +1,11 @@
-def pet_shop_name(name)
-  return "Camelot of Pets"
+def pet_shop_name(pet_shop)
+  return pet_shop[:name]
 end
 
 def total_cash(pet_shop)
   return pet_shop[:admin][:total_cash]
+end
+
+def add_or_remove_cash(pet_shop, money_to_be_added)
+  pet_shop[:admin][:total_cash] += money_to_be_added
 end
